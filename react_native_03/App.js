d7310c045a91d9b844b19c24ad548042dc1dca4d
이유정 <iyujeong@iyujeong-ui-MacBookPro.local>
2021-06-01 02:49:07 +0900

    text Input

---
+++ react_native_03/App.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Input from './src/input';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.mainView}>
+        <Input />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 50,
+    alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  subView: {
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+  },
+  anotherSubView: {
+    flex: 2,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainText: {
+    fontSize: 20,
+    fontWeight: 'normal',
+    color: 'red',
+    padding: 20,
+  },
+});
+
+export default App;
