d972b4d97104ad0aa27edbe538031b120d447b7f
이유정 <iyujeong@iyujeong-ui-MacBookPro.local>
2021-06-02 02:59:42 +0900

    slider

--- react_native_04/src/picker.js
+++ react_native_04/src/picker.js
@@ -9,22 +9,46 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, TextInput} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
+import Slider from '@react-native-community/slider';
 
 class PickerComponent extends Component {
   state = {
     country: 'usa',
+    value: 50,
+  };
+
+  sliderValueChange = value => {
+    this.setState({
+      value: value,
+    });
   };
 
   render() {
     return (
-      <View style={styles.container}>
-        <Picker
-          style={{height: 50, width: 250}}
-          selectedValue={this.state.country}
-          onValueChange={(val, idx) => this.setState({country: val})}>
-          <Picker.Item label="KOREA" value={'korea'} />
-          <Picker.Item label="USA" value={'usa'} />
-        </Picker>
+      <View>
+        <View style={styles.container}>
+          <Slider
+            style={{height: 40, width: 300}}
+            value={this.state.value}
+            minimumValue={0}
+            maximumValue={100}
+            onValueChange={this.sliderValueChange}
+            maximumTrackTintColor={'red'}
+            minimumTrackTintColor={'blue'}
+            step={10}
+          />
+
+          <Picker
+            style={{height: 50, width: 250}}
+            selectedValue={this.state.country}
+            onValueChange={(val, idx) => this.setState({country: val})}>
+            <Picker.Item label="KOREA" value={'korea'} />
+            <Picker.Item label="USA" value={'usa'} />
+          </Picker>
+        </View>
+        <View>
+          <Text style={styles.input}>{this.state.value}</Text>
+        </View>
       </View>
     );
   }
@@ -36,6 +60,13 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     marginBottom: 200,
     alignItems: 'center',
+  },
+  input: {
+    width: '100%',
+    marginTop: 20,
+    fontSize: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
